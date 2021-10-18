32e11d0d607f954381b3344fcf9d7ddc2698fb89
cisco <sovisrushain@gmail.com>
2021-10-18 08:43:36 +0530

    Authenticate the user and save the user token

--- src/sign-in.ts
+++ src/sign-in.ts
@@ -1,4 +1,8 @@
-// localStorage.setItem('token', 'asdsa');
+const BASE_API = 'http://localhost:8080/sms';
+const AUTH_SERVICE_API = BASE_API + '/authenticate';
+
+/* evenet listeners */
+
 
 $('#btn-login').on('click', e => {
     e.preventDefault();
@@ -6,6 +10,28 @@ $('#btn-login').on('click', e => {
     const username =  $('#txtUserName').val() as string;
     const password = $('#txtPassword').val() as string;
 
-    
+    fetch(AUTH_SERVICE_API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body : new URLSearchParams({username: username, password: password}).toString();
+    }).then((response) => {
+        if(response.status === 401){
+            alert('Invalid login credentials');
+            $('#txtUserName').trigger('select');
+        }else if (response.status !== 200){
+            throw new Error('Failed to connect to the server')
+        }else{
+            response.json().then((user) => {
+                localStorage.setItem('token', JSON.stringify(user));
+                window.location.replace('http://localhost:5500/dashboard.html');
+            });
+        }
+    }).catch((err) => {
+        alert(err.message);
+        console.log(err);
+        
+    })
     
 })
